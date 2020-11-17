0428d2dd8a5825ca3bd46527c796107173387ca8
gmasana <gonzalomasana95@gmail.com>
2020-11-17 17:15:33 -0300

    Cambios a las tarjetas de coberturas

--- inicio.php
+++ inicio.php
@@ -4,7 +4,7 @@
             <h1 class="text-center text-color-2 pt-5 slogan d-block">Productores Asesores de Seguros</h1>
         </div>
         <div class="container">
-            <p class="text-center">
+            <p class="text-center pb-2 mb-0">
                 <a href="./?page=contacto" class="btn mx-3 my-2 rounded bg-color-2 text-color-1">Quiero saber más</a>
                 <a href="./?page=siniestro" class="btn mx-3 my-2 rounded bg-color-2 text-color-1">Tuve un siniestro</a>
             </p>        
@@ -15,44 +15,36 @@
     <!--Coberturas-->
     <section>
         <div class="container-fluid pt-3 pb-3 cont_form scroll-horizontal">
-            <div class="card-deck">
-                <div class="card">
-                    <img class="card-img-top" src="imagenes/img-gen.png" alt="Autos">
-                    <div class="card-img-overlay">
-                        <div class="card-body">
-                            <p class="card-title text-center">Autos</p>
+            <div class="row">
+                <div class="col">
+                    <div class="card" style="width:100px;">
+                        <img class="card-img-top" src="imagenes/img-gen.png" alt="Card image">
+                        <div class="card-img-overlay">
+                            <p class="card-text text-center">Autos</p>
                         </div>
                     </div>
                 </div>
-                <div class="card">
-                    <img class="card-img-top" src="imagenes/img-gen.png" alt="ART">
-                    <div class="card-img-overlay">
-                        <div class="card-body">
-                            <p class="card-title text-center">ART</p>
+                <div class="col">
+                    <div class="card" style="width:100px;">
+                        <img class="card-img-top" src="imagenes/img-gen.png" alt="Card image">
+                        <div class="card-img-overlay">
+                            <p class="card-text text-center">ART</p>
                         </div>
                     </div>
                 </div>
-                <div class="card">
-                    <img class="card-img-top" src="imagenes/img-gen.png" alt="Hogar">
-                    <div class="card-img-overlay">
-                        <div class="card-body">
-                            <p class="card-title text-center">Combinado Familiar</p>
+                <div class="col">
+                    <div class="card" style="width:100px;">
+                        <img class="card-img-top" src="imagenes/img-gen.png" alt="Card image">
+                        <div class="card-img-overlay">
+                            <p class="card-text text-center">Hogar</p>
                         </div>
                     </div>
                 </div>
-                <div class="card">
-                    <img class="card-img-top" src="imagenes/img-gen.png" alt="Vida">
-                    <div class="card-img-overlay">
-                        <div class="card-body">
-                            <p class="card-title text-center">Vida</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="card">
-                    <img class="card-img-top" src="imagenes/img-gen.png" alt="Vida">
-                    <div class="card-img-overlay">
-                        <div class="card-body">
-                            <p class="card-title text-center">Incendio</p>
+                <div class="col">
+                    <div class="card" style="width:100px;">
+                        <img class="card-img-top" src="imagenes/img-gen.png" alt="Card image">
+                        <div class="card-img-overlay">
+                            <p class="card-text text-center">Vida</p>
                         </div>
                     </div>
                 </div>
